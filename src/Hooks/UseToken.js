e0932d0223f98c4125419a249afc94218a4d1692
Yasmin Akhter <yasminbd89@yahoo.com>
2022-05-27 18:58:40 +0600

    token added

--- src/Hooks/UseToken.js
+++ src/Hooks/UseToken.js
@@ -20,7 +20,13 @@ const useToken = user => {
                 body: JSON.stringify(currentUser)
             })
                 .then(res => res.json())
-                .then(data => console.log(data))
+                .then(data => {
+                    console.log(data);
+                    const accessToken = data.token;
+                    localStorage.setItem('accessToken', accessToken);
+                    setToken(accessToken)
+
+                })
 
         }
 
